0110974f1c72b1107b8a8726f53e4268fce901e9
kannangr21 <kannanravi2105@gmail.com>
2021-11-24 15:38:17 +0530

    First Commit

---
+++ LayoutTester.java
@@ -0,0 +1,50 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class LayoutTester extends JFrame{
+	public static JLabel l1;
+	public static JButton b1 = new JButton("1"), b2 = new JButton("2"), b3 = new JButton("3"),
+	b4 = new JButton("4"), b5 = new JButton("5"), b6 = new JButton("6"),
+	b7 = new JButton("7"), b8 = new JButton("8"), b9 = new JButton("9"),
+	bs = new JButton("+"), b0 = new JButton("0"), bb = new JButton("Del"),
+	s1 = new JButton("Sim 1"), s2 = new JButton("Sim 2"), lo = new JButton("Logs");
+	
+	public LayoutTester(){
+		setTitle("My Test Application");
+		setBounds(100,100,400,625);
+		Container pane = getContentPane();
+		pane.setLayout(new BorderLayout(50,50));
+		
+		l1 = new JLabel("", SwingConstants.CENTER);
+		l1.setFont(new java.awt.Font("Arial",java.awt.Font.BOLD,25));
+		
+		JPanel nums = new JPanel(new GridLayout(4, 3, 15, 25));
+		
+		nums.add(b1);nums.add(b2);nums.add(b3);
+		nums.add(b4);nums.add(b5);nums.add(b6);
+		nums.add(b7);nums.add(b8);nums.add(b9);
+		nums.add(bs);nums.add(b0);nums.add(bb);
+		
+		JPanel keys = new JPanel(new FlowLayout());
+		keys.add(s1); keys.add(s2); keys.add(lo);
+		
+		pane.add(l1,"North");
+		pane.add(nums, "Center");
+		pane.add(keys, "South");
+		// AddNumber a = new AddNumber();
+		b1.addActionListener(new AddNumber());
+		b2.addActionListener(new AddNumber());
+		b3.addActionListener(new AddNumber());
+		b4.addActionListener(new AddNumber());
+		b5.addActionListener(new AddNumber());
+		b6.addActionListener(new AddNumber());
+		b7.addActionListener(new AddNumber());
+		b8.addActionListener(new AddNumber());
+		b9.addActionListener(new AddNumber());
+		b0.addActionListener(new AddNumber());
+		bs.addActionListener(new AddNumber());
+		bb.addActionListener(new AddNumber());
+	}
+}
+
---
+++ SwingExampleOne.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import javax.swing.table.*;
+import java.awt.*;
+
+public class SwingExampleOne{
+	public static void main(String args[]){
+		JFrame f = new LayoutTester();
+		/*
+		f.setSize(500,725);
+		f.setLayout(new BorderLayout(4,1)); 		
+		JLabel l = new JLabel("Sir eh massuh!", SwingConstants.CENTER);
+		JButton b1 = new JButton("Oru Button");
+		JButton b2 = new JButton("2 Button");
+		JButton b3 = new JButton("3 Button");
+		JButton b4 = new JButton("4 Button");
+		f.add(l);
+		f.add(b1);
+		f.add(b2);
+		f.add(b3);
+		f.add(b4);
+		*/
+		f.setVisible(true);
+	}
+}
