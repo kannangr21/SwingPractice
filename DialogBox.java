0c2dc88ec8246eaf65422052849c233f02707cc6
kannangr21 <kannanravi2105@gmail.com>
2021-11-26 00:46:50 +0530

    Added Call interface

---
+++ DialogBox.java
@@ -0,0 +1,33 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class DialogBox implements  ActionListener{
+	public void actionPerformed(ActionEvent e){
+		JFrame box = new JFrame("Call Operation");
+		box.setBounds(350,350,200,150);
+		box.setLayout(new BorderLayout());
+		JLabel label = new JLabel("");
+		if(e.getActionCommand() == "Hold"){
+			label.setText("Your call is on hold!");
+		}
+		else if(e.getActionCommand() == "Add Call"){
+			label.setText("You can add calls in a mobile");
+		}
+		else if(e.getActionCommand() == "Record"){
+			label.setText("Call Recording.....");
+		}
+		else if(e.getActionCommand() == "Speaker"){
+			label.setText("Speakers On! Can you hear me??");
+		}
+		JButton back = new JButton("Back");
+		back.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				box.dispatchEvent(new WindowEvent(box, WindowEvent.WINDOW_CLOSING));
+			}
+		});
+		box.add(label, "Center");
+		box.add(back, "South");
+		box.setVisible(true);
+	}
+}
--- LayoutTester.java
+++ LayoutTester.java
@@ -9,17 +9,21 @@ public class LayoutTester extends JFrame{
 	b7 = new JButton("7"), b8 = new JButton("8"), b9 = new JButton("9"),
 	bs = new JButton("+"), b0 = new JButton("0"), bb = new JButton("Del"),
 	s1 = new JButton("Sim 1"), s2 = new JButton("Sim 2"), lo = new JButton("Logs");
-	
+	public static JPanel nums = new JPanel(new GridLayout(4, 3, 10, 10));
 	public LayoutTester(){
 		setTitle("My Test Application");
-		setBounds(100,100,400,625);
+		setBounds(100,100,300,425);
 		Container pane = getContentPane();
 		pane.setLayout(new BorderLayout(50,50));
 		
 		l1 = new JLabel("", SwingConstants.CENTER);
 		l1.setFont(new java.awt.Font("Arial",java.awt.Font.BOLD,25));
 		
-		JPanel nums = new JPanel(new GridLayout(4, 3, 15, 25));
+		
+		
+		buttonStyle(b1,0);buttonStyle(b2,0);buttonStyle(b3,0);buttonStyle(b4,0);
+		buttonStyle(b5,0);buttonStyle(b6,0);buttonStyle(b7,0);buttonStyle(b8,0);
+		buttonStyle(b9,0);buttonStyle(b0,0);buttonStyle(bb,0);buttonStyle(bs,0);
 		
 		nums.add(b1);nums.add(b2);nums.add(b3);
 		nums.add(b4);nums.add(b5);nums.add(b6);
@@ -32,7 +36,7 @@ public class LayoutTester extends JFrame{
 		pane.add(l1,"North");
 		pane.add(nums, "Center");
 		pane.add(keys, "South");
-		// AddNumber a = new AddNumber();
+		
 		b1.addActionListener(new AddNumber());
 		b2.addActionListener(new AddNumber());
 		b3.addActionListener(new AddNumber());
@@ -45,6 +49,18 @@ public class LayoutTester extends JFrame{
 		b0.addActionListener(new AddNumber());
 		bs.addActionListener(new AddNumber());
 		bb.addActionListener(new AddNumber());
+		s1.addActionListener(new Calling());
+		s2.addActionListener(new Calling());
+		//s2.addActionListener(new Calling());
+	}
+	public static void buttonStyle(JButton jb, int i){
+		jb.setBorder(BorderFactory.createEmptyBorder());
+		jb.setOpaque(false);
+		jb.setContentAreaFilled(false);
+        jb.setBorderPainted(false);
+        jb.setFocusPainted(false);
+		if(i == 0)
+		jb.setFont(new java.awt.Font("Arial",java.awt.Font.BOLD,27));		
 	}
 }
 
